0c10cce83a5fa067a3765dd41001f7ab381a19b8
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2023-02-22 12:13:13 +0900

    test

---
+++ src/items/example.spec.ts
@@ -0,0 +1,8 @@
+describe('Jestの練習', () => {
+  // 1it 1expect
+  it('test1', () => {
+    const result = 1;
+    const expected = 1;
+    expect(result).toEqual(expected);
+  });
+});
---
+++ src/items/items.service.spec.ts
@@ -0,0 +1,173 @@
+// nestjsのサービス、レポジトリのインスタンス化で使う
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { UserStatus } from '../auth/user-status.enum';
+import { ItemStatus } from './item-status.enum';
+import { ItemRepository } from './item.repository';
+import { ItemsService } from './items.service';
+
+// mock
+const mockItemRepository = () => ({
+  // mock関数の定義
+  find: jest.fn(),
+  findOne: jest.fn(),
+  createItem: jest.fn(),
+  save: jest.fn(),
+  delete: jest.fn(),
+});
+
+const mockUser1 = {
+  id: '1',
+  name: 'test1',
+  password: '1234',
+  status: UserStatus.PREMIUM,
+};
+
+const mockUser2 = {
+  id: '2',
+  name: 'test2',
+  password: '1234',
+  status: UserStatus.FREE,
+};
+
+// test
+describe('ItemsServiceTest', () => {
+  let itemsService;
+  let itemRepository;
+  // テスト前に実行する関数
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        ItemsService,
+        {
+          provide: ItemRepository,
+          useFactory: mockItemRepository,
+        },
+      ],
+    }).compile();
+
+    itemsService = module.get<ItemsService>(ItemsService);
+    itemRepository = module.get<ItemRepository>(ItemRepository);
+  });
+
+  describe('findAll', () => {
+    it('正常系', async () => {
+      const expected = [];
+      itemRepository.find.mockResolvedValue(expected);
+      const result = await itemsService.findAll();
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('findById', () => {
+    it('正常系', async () => {
+      const expected = {
+        id: 'test-id',
+        name: 'PC',
+        price: 50000,
+        description: '',
+        status: ItemStatus.ON_SALE,
+        createdAt: '',
+        updatedAt: '',
+        userId: mockUser1.id,
+        user: mockUser1,
+      };
+
+      itemRepository.findOne.mockResolvedValue(expected);
+      const result = await itemsService.findById('test-id');
+      expect(result).toEqual(expected);
+    });
+
+    it('異常系: 商品が存在しない', async () => {
+      itemRepository.findOne.mockResolvedValue(null);
+      await expect(itemsService.findById('test-id')).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('createItem', () => {
+    it('正常系', async () => {
+      const expected = {
+        id: 'test-id',
+        name: 'PC',
+        price: 50000,
+        description: '',
+        status: ItemStatus.ON_SALE,
+        createdAt: '',
+        updatedAt: '',
+        userId: mockUser1.id,
+        user: mockUser1,
+      };
+
+      itemRepository.createItem.mockResolvedValue(expected);
+      const result = await itemsService.create(
+        {
+          name: 'PC',
+          price: 50000,
+          description: '',
+        },
+        mockUser1,
+      );
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('updateStatus', () => {
+    const mockItem = {
+      id: 'test-id',
+      name: 'PC',
+      price: 50000,
+      description: '',
+      status: ItemStatus.ON_SALE,
+      createdAt: '',
+      updatedAt: '',
+      userId: mockUser1.id,
+      user: mockUser1,
+    };
+
+    it('正常系', async () => {
+      itemRepository.findOne.mockResolvedValue(mockItem);
+      await itemsService.updateStatus('test-id', mockUser2);
+      // 関数が呼び出されたら成功
+      expect(itemRepository.save).toHaveBeenCalled();
+    });
+
+    it('異常系: 自身の商品を購入', async () => {
+      itemRepository.findOne.mockResolvedValue(mockItem);
+      await expect(
+        itemsService.updateStatus('test-id', mockUser1),
+      ).rejects.toThrow(BadRequestException);
+    });
+  });
+
+  describe('delete', () => {
+    const mockItem = {
+      id: 'test-id',
+      name: 'PC',
+      price: 50000,
+      description: '',
+      status: ItemStatus.ON_SALE,
+      createdAt: '',
+      updatedAt: '',
+      userId: mockUser1.id,
+      user: mockUser1,
+    };
+
+    it('正常系', async () => {
+      itemRepository.findOne.mockResolvedValue(mockItem);
+      await itemsService.delete('test-id', mockUser1);
+      // 関数が呼び出されたら成功
+      expect(itemRepository.delete).toHaveBeenCalled();
+    });
+
+    it('異常系: 他人の商品を削除', async () => {
+      itemRepository.findOne.mockResolvedValue(mockItem);
+      await expect(itemsService.delete('test-id', mockUser2)).rejects.toThrow(
+        BadRequestException,
+      );
+    });
+  });
+});
